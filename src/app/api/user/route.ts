57b39b323bf555be65996fdb728bd40d5d359114
Jude Francis Igot <34589286+judigot@users.noreply.github.com>
2024-01-16 15:14:03 +0800

    Update route.ts

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,17 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
+import {NextRequest, NextResponse} from 'next/server';
 
-import GetHandler from "./Get";
-import PostHandler from "./Post";
+import GetHandler from './Get';
+import PostHandler from './Post';
 // import PatchHandler from "./Patch";
 // import PutHandler from "./Put";
 // import DeleteHandler from "./Delete";
 // import HeadHandler from "./Head";
 // import OptionsHandler from "./Options";
 
-export const GET = async (req: NextRequest) => GetHandler(req);
-export const POST = async (req: NextRequest) => PostHandler(req);
-// export const PATCH = async (req: NextRequest) => PatchHandler(req);
-// export const PUT = async (req: NextRequest) => PutHandler(req);
-// export const DELETE = async (req: NextRequest) => DeleteHandler(req);
-// export const HEAD = async (req: NextRequest) => HeadHandler(req);
-// export const OPTIONS = async (req: NextRequest) => OptionsHandler(req);
+export async function GET(req: NextRequest) {
+  return GetHandler(req);
+}
+export async function POST(req: NextRequest) {
+  return GetHandler(req);
+}
+// export async function PATCH (req: NextRequest) {PatchHandler(req)};
+// export async function PUT (req: NextRequest) {PutHandler(req)};
+// export async function DELETE (req: NextRequest) {DeleteHandler(req)};
+// export async function HEAD (req: NextRequest) {HeadHandler(req)};
+// export async function OPTIONS (req: NextRequest) {OptionsHandler(req)};
